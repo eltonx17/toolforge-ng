490dbf0c85e6b44c205e02f2a694192e454564ae
Elton Gonsalves <eltonx17@gmail.com>
2025-03-21 19:25:16 +0530

    [0.0.0] - Add JSON Format & Validation

--- src/app/components/editor-panel/editor-panel.component.ts
+++ src/app/components/editor-panel/editor-panel.component.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { ThemeService } from '../../services/theme.service';
+import { ClarityModule } from '@clr/angular';
 
 @Component({
   selector: 'app-editor-panel',
-  imports: [FormsModule, RouterModule, MonacoEditorModule],
+  imports: [FormsModule, RouterModule, MonacoEditorModule, ClarityModule],
   templateUrl: './editor-panel.component.html',
   styleUrls: ['./editor-panel.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -18,8 +19,56 @@ export class EditorPanelComponent implements OnInit {
     language: 'text',
     theme: 'vs-light',
     minimap: { enabled: false },
+    scrollBeyondLastLine: false,
+    lineHeight: 20,
+    //fontSize: 14,
+    wordWrap: 'on',
+    wrappingIndent: 'indent',
   };
-  code = ``;
+  code = `{
+  "simple key": "simple value",
+  "numbers": 1234567,
+  "simple list": [
+    "value1",
+    22222,
+    "value3"
+  ],
+  "owner": null,
+  "simple obect": {
+    "simple key": "simple value",
+    "numbers": 1234567,
+    "simple list": [
+      "value1",
+      22222,
+      "value3"
+    ],
+    "simple obect": {
+      "key1": "value1",
+      "key2": 22222,
+      "key3": "value3"
+    }
+  }
+}`;
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.code).then(() => {
+      const copyButton = document.querySelector('.tool-button .btn');
+      if (copyButton) {
+        copyButton.textContent = 'Copied!';
+        setTimeout(() => {
+          copyButton.textContent = 'Copy';
+        }, 2000);
+      }
+    }).catch(err => {
+      // Do nothing on error
+    });
+  }
+
+  toggleWordWrap() {
+    this.editorOptions.wordWrap = this.editorOptions.wordWrap === 'on' ? 'off' : 'on';
+    console.log('Word Wrap toggled to:', this.editorOptions.wordWrap);
+    this.editorOptions = { ...this.editorOptions }; 
+  }
 
   constructor(private themeService: ThemeService) {}
 
--- src/app/components/formatter/formatter.component.ts
+++ src/app/components/formatter/formatter.component.ts
@@ -1,21 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EditorPanelComponent } from '../editor-panel/editor-panel.component';
-import { ClarityModule } from '@clr/angular';
+import { ClarityModule, ClrAlertModule } from '@clr/angular';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-formatter',
-  imports: [RouterModule, EditorPanelComponent, ClarityModule],
+  imports: [RouterModule, EditorPanelComponent, ClarityModule, ClrAlertModule, CommonModule],
   templateUrl: './formatter.component.html',
   styleUrl: './formatter.component.css'
 })
 export class FormatterComponent {
   selectedJsonMethod: string = 'JSON Validator';
+  @ViewChild(EditorPanelComponent) editorPanel!: EditorPanelComponent;
+
+  showAlert: boolean = false;
+  alertMessage: string = '';
+  alertType: 'success' | 'danger' = 'success';
 
   onHashMethodSelect(event: Event) {
     const method = (event.target as HTMLElement).textContent?.trim();
     if (method) {
       this.selectedJsonMethod = method;
     }
+  }
+
+  validateAndFormatJson() {
+    try {
+      const parsedJson = JSON.parse(this.editorPanel.code);
+      this.editorPanel.code = JSON.stringify(parsedJson, null, 2);
+      this.alertMessage = 'Valid JSON';
+      this.alertType = 'success';
+    } catch (e) {
+      this.alertMessage = 'Invalid JSON';
+      this.alertType = 'danger';
+    }
+    this.showAlert = true;
+    console.log('showAlert set to true');
+    setTimeout(() => {
+      this.showAlert = false;
+      console.log('showAlert set to false');
+    }, 2000);
   }
 }
